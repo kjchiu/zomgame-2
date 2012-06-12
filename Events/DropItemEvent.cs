543fed86cefd155e9a1083dcaa5586e564860438
kjchiu <krypes+github@gmail.com>
2012-06-11 20:47:30 -0400

    Throw's InvalidOperation if somehow drop event triggers for an item not in player's inventory.

--- Events/DropItemEvent.cs
+++ Events/DropItemEvent.cs
@@ -22,12 +22,18 @@ namespace Zomgame.Events
 
         public override void fireEvent()
         {
+            if (!picker.Inventory.Contains(item))
+            {
+                throw new InvalidOperationException("Tried to drop item not in player's inventory.");
+            }
+            
             Trace.WriteLine(String.Format("{0} is dropping the {1}", picker.Name, item.Name));
 
             if (picker.ItemIsEquipped(item))
             {
                 EventHandler.Instance.AddEvent(EventFactory.CreateUnequipItemEvent(picker, item));
             }
+            
             picker.State = Creature.EntityState.IDLE;
 
             picker.Location.AddObject(item);
